bd8e452881137d392cb8a05556d7c65096710a0f
raytheboffin <111377374+josephchikak@users.noreply.github.com>
2023-06-23 16:08:29 +0300

    deploy

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import {BrowserRouter} from "react-router-dom";
 
 
@@ -9,7 +9,7 @@ import {BrowserRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <BrowserRouter basename='.'>
+    <BrowserRouter basename='/'>
        <App /> 
     </BrowserRouter>
 
